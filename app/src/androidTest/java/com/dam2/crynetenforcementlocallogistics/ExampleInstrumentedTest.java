d4efcd2b23d3f506da85da460c98c4953abcdc3b
ajuanjojjj <ajuanjojjj@users.noreply.github.com>
2019-06-09 19:39:18 +0200

    Añadir nuevos iconos GPU

--- app/src/androidTest/java/com/dam2/crynetenforcementlocallogistics/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dam2/crynetenforcementlocallogistics/ExampleInstrumentedTest.java
@@ -1,12 +1,22 @@
 package com.dam2.crynetenforcementlocallogistics;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -26,6 +36,28 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.dam2.crynetenforcementlocallogistics", appContext.getPackageName());
     }
+    
+    @Test
+    public void checkLogin(){
+        String[] emailGud = {"jhargreave@hrbiomed.corp"};
+        String[] passGud = {"PASSDETEST"};
+        String[] emailBad = {"asdasdasdas@gmail.com"};
+        String[] passBad = {"dasdasasdasd"};
+        for (int i = 0; i < 1; i++)
+            assertNotEquals(checkLogin(emailGud[i], passGud[i]), null);
+
+        for (int i = 0; i < 1; i++)
+            assertEquals(checkLogin(emailBad[i], passBad[i]), null);
+    }
+
+    public String checkLogin(String email, String pass){
+        UserLoginTask mAuthTask = new UserLoginTask();
+        try {
+            return mAuthTask.execute(email, pass).get();
+        } catch (Exception e){
+            return null;
+        }
+    }
 
     @Test
     public void isMatrixGood(){
@@ -35,15 +67,6 @@ public class ExampleInstrumentedTest {
         double deviation = standardDeviation(result);
 
         assertTrue(deviation < 10);
-    }
-
-    @Test
-    public void checkKeypadWorks() {
-        checkKeypadWorks(new Random().nextLong());
-    }
-
-    public void checkKeypadWorks(long key) {
-
     }
 
     public long[] countMatrixOcurrences(int sample) {
@@ -93,4 +116,57 @@ public class ExampleInstrumentedTest {
     }
 
 
+    /**
+     * Represents an asynchronous login/registration task used to authenticate the user.
+     */
+    class UserLoginTask extends AsyncTask<String, Void, String> {
+
+        private static final String PASSWORD = "password";   //NON-NLS
+        private static final String EMAIL = "email";         //NON-NLS
+        private static final String URI = "http://crynet.wunderapp.es/servicios/login";
+        private String mEmail;
+        private String mPassword;
+        /*
+                UserLoginTask(String email, String password) {
+                    mEmail = email;
+                    mPassword = password;
+                }
+        */
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL(URI);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.setRequestProperty("Content-Type", "application/" + "json");
+                    urlConnection.setDoOutput(true);
+                    //urlConnection.setRequestProperty("Authorization", "Bearer " + App.token);
+
+                    String json = "{\"login\": \"" + params[0] + "\",\"password\":\"" + params[1] + "\"}";
+                    byte[] outputInBytes = json.getBytes(StandardCharsets.UTF_8);
+
+
+                    OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                    out.write( outputInBytes );
+                    out.close();
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8));
+                    String token =  in.readLine().replace("\"", "");
+                    return token;
+
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception ex) {
+                Log.e("Error en login", ex.getMessage());
+                //int responseCode = urlConnection.getResponseCode(); //can call this instead of con.connect()
+                /*if (responseCode >= 400 && responseCode <= 499) {
+                    throw new Exception("Bad authentication status: " + responseCode); //provide a more meaningful exception message
+                }*/
+                return null;
+            }
+        }
+    }
 }
